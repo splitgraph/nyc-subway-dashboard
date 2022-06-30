49492f8a205d693d94827c70f6b4455cc174b36e
Patrick Skinner <pas8314@gmail.com>
2022-06-30 03:26:09 +0300

    plot out weekly totals

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 import altair as alt
 import math
 import pandas as pd
@@ -15,3 +15,51 @@ conn = psycopg2.connect(
     database = st.secrets["ddn"]["database"],
 )
 
+# Layout
+row1_1, row1_2 = st.columns((2, 3))
+
+with row1_1:
+    st.title('NYC Covid Recovery')
+
+with row1_2:
+    st.write(
+        """
+        ##
+        Plotting subway turnstile data to show how life might be returning to normal in New York City.
+        """
+    )
+
+def weeklytotals():
+    data_load_state = st.text('Loading data from Splitgraph DDN...')
+    with conn:
+        with conn.cursor() as curs:
+            curs.execute("""SELECT "turnstile-activity"."START_DATE", SUM("turnstile-activity"."TOTAL") 
+                FROM "paws/nyc-transit"."turnstile-activity"
+                GROUP BY "turnstile-activity"."START_DATE"
+                """
+            )
+            result = curs.fetchall()
+    
+    data_load_state = st.success('Done!')
+
+    totals = defaultdict(int)
+    for (date, total) in result:
+        if (total < 600000000): # there are 3 (likely) spurious spikes in the data - exclude them
+            totals[date] += total
+    
+    data = {"DATE": [], "TOTAL": []}
+    for k, v in totals.items():
+        data['DATE'].append(k)
+        data['TOTAL'].append(v)
+    
+    source = pd.DataFrame.from_dict(data)
+    line_chart = alt.Chart(source).mark_line().encode(
+            alt.X('DATE:T', title='Time'),
+            alt.Y('TOTAL:Q', title='Weekly rides'),
+        ).properties(title="MTA total weekly turnstile activity (Jan 2020-May 2022)")
+        
+    st.altair_chart(line_chart.configure_title(fontSize=24), use_container_width=True)
+    st.write("Feburary 2020 has more than 60m weekly rides. By May 2022, we see a little over 30m.")
+
+
+weeklytotals()
